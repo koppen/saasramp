2db1868d934589c81239abd54e9534960f235839
Jakob Skjerning <jakob@mentalized.net>
2011-03-01 14:55:04 +0100

    Don't attempt to send email if it'd result in a failure - primarily a possibility in tests

--- lib/subscription_transaction_observer.rb
+++ lib/subscription_transaction_observer.rb
@@ -8,17 +8,18 @@ class SubscriptionTransactionObserver < ActiveRecord::Observer
   
   def after_save(transaction)
     sub = transaction.subscription
+    send_mail = sub && sub.subscriber && !sub.subscriber.email.blank?
     case transaction.action
     when 'charge'
       if transaction.success?
-        SubscriptionConfig.mailer.deliver_charge_success(sub, transaction)
+        SubscriptionConfig.mailer.deliver_charge_success(sub, transaction) if send_mail
       else 
         sub.increment!(:warning_level)
         case sub.warning_level
         when 1
-          SubscriptionConfig.mailer.deliver_charge_failure(sub, transaction)
+          SubscriptionConfig.mailer.deliver_charge_failure(sub, transaction) if send_mail
         when 2
-          SubscriptionConfig.mailer.deliver_second_charge_failure(sub, transaction)
+          SubscriptionConfig.mailer.deliver_second_charge_failure(sub, transaction) if send_mail
         else
           # expired: do in the app whatever it means to become expired
           # send no mail here
@@ -27,7 +28,7 @@ class SubscriptionTransactionObserver < ActiveRecord::Observer
       
     when 'credit', 'refund'
       if transaction.success?
-        SubscriptionConfig.mailer.deliver_credit_success(sub, transaction)
+        SubscriptionConfig.mailer.deliver_credit_success(sub, transaction) if send_mail
       end
       # else no email
       
